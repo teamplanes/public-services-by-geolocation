c1bb1a9d8e746895c31f7423817c4065a7636995
Hennerz <henry@kirkness.com>
2022-06-30 11:18:06 +0100

    read from heroku port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const AppError = require("./src/lib/Error");
 
 require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
